588dabf4df0b096132a84523aeffbf252508c384
paul-achard <paulchardlyon@gmail.com>
2021-01-04 14:07:46 +0100

    fix du canvas

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const HEXTILES_IMAGE = new Image();
-HEXTILES_IMAGE.src = '../images/fantasyhextiles_v3.png';
+HEXTILES_IMAGE.src = '../images/hextiles.png';
 Promise.all([
-    new Promise( (resolve) => {HEXTILES_IMAGE.addEventListener('load', () => { resolve();}); })
-])
-    .then(() => {
+    new Promise((resolve) => {
+        HEXTILES_IMAGE.addEventListener('load', () => {
+            resolve();
+        });
+    })
+]).then(() => {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
-        var image = document.getElementById("source");
-
-        ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+        ctx.drawImage(HEXTILES_IMAGE, 0, 0);
     });
