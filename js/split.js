aaf92020ca9a6967500c732a39bc36361884728b
clement-galzi <clementgalzi@gmail.com>
2021-01-15 16:32:55 +0100

    printHex

--- js/split.js
+++ js/split.js
@@ -6,7 +6,7 @@ let image = document.getElementById("source");
 // coordX où on veut mettre la tile,
 // coordY où on veut mettre la tile, 32, 48);
 // ex : ctx.drawImage(image, 32, 16, 32, 48, 50, 0, 32, 48);
-ctx.drawImage(image, 0, 16, 32, 48, 100, 50, 32, 48);
+//ctx.drawImage(image, 0, 16, 32, 48, 100, 50, 32, 48);
 
 function initTabHex(){
     let tab = [];
@@ -59,9 +59,30 @@ function initTabHex(){
     }
     return tab;
 }
-console.log(initTabHex());
-function printHex(idHex, coordX, coordY){
-
-}
 let tab = initTabHex();
+console.log(initTabHex());
+//Find return undefined quand il ne trouve pas la valeur
+function getCoordFromId(idHex){
+    let found;
+    let coordX;
+    let coordY;
+    for (let i=0; i<10; i++){
+        found = tab[i].find(element => element === idHex);
+        if (found !== undefined){
+            coordY = i;
+            coordX = idHex - i*16;
+        }
+    }
+    return [coordX, coordY];
+}
+function printHex(idHex, coordX, coordY){
+    let coord = getCoordFromId(idHex);
+    ctx.drawImage(image, coord[0]*32, coord[1]*48, 32, 48, coordX, coordY, 32, 48);
+}
+for (let i=24; i<50;i++){
+    printHex(147,32*i,50)
+}
+
+
+
 console.log(tab);
